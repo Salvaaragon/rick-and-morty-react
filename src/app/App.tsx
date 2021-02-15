145183607a313f381b55c1b6f6935e3d7e3da7b2
Salva Aragón Reyes <salva.aragonreyes@gmail.com>
2021-02-15 21:04:52 +0100

    feat: set language loader into index app file

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,33 +5,23 @@ import {
 import { useQuery } from '@apollo/client';
 import BackgroundImage from '@Assets/images/background.png';
 import Layout from '@Components/Layout';
-import { Grid, MuiThemeProvider } from '@material-ui/core';
-import i18n from '@Services/i18n/config';
-import theme from '@Utils/theme';
-import React, { Suspense } from 'react';
-import { I18nextProvider } from 'react-i18next';
+import { Grid } from '@material-ui/core';
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
 
-const App: () => JSX.Element = () => {
+type AppType = () => JSX.Element;
+
+const App: AppType = () => {
   const { data } = useQuery<CharactersData>(GET_CHARACTERS_QUERY);
 
   console.log(data);
 
   return (
-    <Suspense fallback={null}>
-      <I18nextProvider i18n={i18n}>
-        <MuiThemeProvider theme={theme}>
-          <ThemeProvider theme={theme}>
-            <Grid style={{ backgroundImage: `url(${BackgroundImage})` }}>
-              <Router>
-                <Layout />
-              </Router>
-            </Grid>
-          </ThemeProvider>
-        </MuiThemeProvider>
-      </I18nextProvider>
-    </Suspense>
+    <Grid style={{ backgroundImage: `url(${BackgroundImage})` }}>
+      <Router>
+        <Layout />
+      </Router>
+    </Grid>
   );
 };
 
