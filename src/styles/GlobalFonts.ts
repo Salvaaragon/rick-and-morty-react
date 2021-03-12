1750fb277f6fd59b02dbdb7005fdadba1958063f
Salva Aragón Reyes <salva.aragonreyes@gmail.com>
2021-03-12 13:41:39 +0100

    feat: configure fonts for use in styled-components and material ui

---
+++ src/styles/GlobalFonts.ts
@@ -0,0 +1,112 @@
+import RalewayBlack from '@Assets/fonts/raleway/Raleway-Black.ttf';
+import RalewayBlackItalic from '@Assets/fonts/raleway/Raleway-BlackItalic.ttf';
+import RalewayBold from '@Assets/fonts/raleway/Raleway-Bold.ttf';
+import RalewayBoldItalic from '@Assets/fonts/raleway/Raleway-BoldItalic.ttf';
+import RalewayExtraBold from '@Assets/fonts/raleway/Raleway-ExtraBold.ttf';
+import RalewayExtraBoldItalic from '@Assets/fonts/raleway/Raleway-ExtraBoldItalic.ttf';
+import RalewayExtraLight from '@Assets/fonts/raleway/Raleway-ExtraLight.ttf';
+import RalewayExtraLightItalic from '@Assets/fonts/raleway/Raleway-ExtraLightItalic.ttf';
+import RalewayItalic from '@Assets/fonts/raleway/Raleway-Italic.ttf';
+import RalewayLight from '@Assets/fonts/raleway/Raleway-Light.ttf';
+import RalewayLightItalic from '@Assets/fonts/raleway/Raleway-LightItalic.ttf';
+import RalewayMedium from '@Assets/fonts/raleway/Raleway-Medium.ttf';
+import RalewayMediumItalic from '@Assets/fonts/raleway/Raleway-MediumItalic.ttf';
+import RalewayRegular from '@Assets/fonts/raleway/Raleway-Regular.ttf';
+import RalewaySemiBold from '@Assets/fonts/raleway/Raleway-SemiBold.ttf';
+import RalewaySemiBoldItalic from '@Assets/fonts/raleway/Raleway-SemiBoldItalic.ttf';
+import RalewayThin from '@Assets/fonts/raleway/Raleway-Thin.ttf';
+import RalewayThinItalic from '@Assets/fonts/raleway/Raleway-ThinItalic.ttf';
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    @font-face {
+        font-family: 'Raleway Black';
+        src: url(${RalewayBlack});
+        font-style: 'normal';
+    }
+    @font-face {
+        font-family: 'Raleway Black Italic';
+        src: url(${RalewayBlackItalic});
+        font-style: 'normal';
+    }
+    @font-face {
+        font-family: 'Raleway Bold';
+        src: url(${RalewayBold});
+        font-style: 'normal';
+    }
+    @font-face {
+        font-family: 'Raleway Bold Italic';
+        src: url(${RalewayBoldItalic});
+        font-style: 'normal';
+    }
+    @font-face {
+        font-family: 'Raleway Extra Bold';
+        src: url(${RalewayExtraBold});
+        font-style: 'normal';
+    }
+    @font-face {
+        font-family: 'Raleway Extra Bold Italic';
+        src: url(${RalewayExtraBoldItalic});
+        font-style: 'normal';
+    }
+    @font-face {
+        font-family: 'Raleway Extra Light';
+        src: url(${RalewayExtraLight});
+        font-style: 'normal';
+    }
+    @font-face {
+        font-family: 'Raleway Extra Light Italic';
+        src: url(${RalewayExtraLightItalic});
+        font-style: 'normal';
+    }
+    @font-face {
+        font-family: 'Raleway Italic';
+        src: url(${RalewayItalic});
+        font-style: 'normal';
+    }
+    @font-face {
+        font-family: 'Raleway Light';
+        src: url(${RalewayLight});
+        font-style: 'normal';
+    }
+    @font-face {
+        font-family: 'Raleway Light Italic';
+        src: url(${RalewayLightItalic});
+        font-style: 'normal';
+    }
+    @font-face {
+        font-family: 'Raleway Medium';
+        src: url(${RalewayMedium});
+        font-style: 'normal';
+    }
+    @font-face {
+        font-family: 'Raleway Medium Italic';
+        src: url(${RalewayMediumItalic});
+        font-style: 'normal';
+    }
+    @font-face {
+        font-family: 'Raleway';
+        src: url(${RalewayRegular});
+        font-style: 'normal';
+    }
+    @font-face {
+        font-family: 'Raleway SemiBold';
+        src: url(${RalewaySemiBold});
+        font-style: 'normal';
+    }
+    @font-face {
+        font-family: 'Raleway SemiBold Italic';
+        src: url(${RalewaySemiBoldItalic});
+        font-style: 'normal';
+    }
+    @font-face {
+        font-family: 'Raleway Thin';
+        src: url(${RalewayThin});
+        font-style: 'normal';
+    }
+    @font-face {
+        font-family: 'Raleway Thin Italic';
+        src: url(${RalewayThinItalic});
+        font-style: 'normal';
+    }
+`;
